1573b529f8f2a71604302877f850f7c3311f5e7b
geogrey12muiruri <greggambugua@gmail.com>
2023-07-30 16:54:55 +0300

    modified the form

--- src/scenes/home/FancyForm.jsx
+++ src/scenes/home/FancyForm.jsx
@@ -1,6 +1,8 @@
-
-import { useState } from 'react';
-
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 
 const FancyForm = () => {
   const [formData, setFormData] = useState({
@@ -24,44 +26,65 @@ const FancyForm = () => {
   };
 
   return (
-    <div className="fancy-form-container">
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh', // Set the height to full viewport height
+        width: '100%', // Set the width to full viewport width
+        backgroundColor: (theme) => theme.palette.background.paper,
+      }}
+    >
       <h1>Fancy Form</h1>
-      <form className="fancy-form" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
+      <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+        <Box sx={{ mb: 2 }}>
+          <TextField
             type="text"
             id="name"
             name="name"
+            label="Name"
             value={formData.name}
             onChange={handleChange}
+            variant="outlined"
+            fullWidth
             required
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
+        </Box>
+        <Box sx={{ mb: 2 }}>
+          <TextField
             type="email"
             id="email"
             name="email"
+            label="Email"
             value={formData.email}
             onChange={handleChange}
+            variant="outlined"
+            fullWidth
             required
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message:</label>
-          <textarea
+        </Box>
+        <Box sx={{ mb: 2 }}>
+          <TextareaAutosize
             id="message"
             name="message"
             value={formData.message}
             onChange={handleChange}
+            placeholder="Message"
+            rows={4}
+            variant="outlined"
+            fullWidth
             required
           />
-        </div>
-        <button type="submit">Submit</button>
+        </Box>
+        <Box sx={{ my: 2 }}>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </Box>
       </form>
-    </div>
+    </Box>
   );
 };
 
